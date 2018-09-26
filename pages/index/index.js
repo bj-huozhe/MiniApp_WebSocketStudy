bb360f8db8e06a3334cf29333cf507ff29563ae0
zlpk2018 <zlpk2018@gmail.com>
2018-09-27 01:05:58 +0800

    Go + 小程序实现 websocket 相互收发消息。

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,90 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    outputContent: [],
+    message: "",
+    socketTask: null
   },
+
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  /**
+   * 输入消息
+   */
+  inputTap: function(e) {
+    this.setData({message: e.detail.value});
+  },
+
+  /**
+   * 建立 WebSocket 连接
+   */
+  openConnection:function() {
+    let that = this;
+    var socketTask = wx.connectSocket({
+      url: 'ws://localhost:8080/ws',
+      success: function() {
+        var tempContent = that.data.outputContent;
+        tempContent.push('建立 WebSocket 成功!');
+        that.setData({
+          outputContent: tempContent,
+        })
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET"
+    })
+
+    that.setData({ 
+      socketTask: socketTask
+      })
+
+    // 监听 WebSocket 接受到服务器的消息事件
+    socketTask.onMessage(onMessage => {
+      var resvMsg = onMessage.data;
+      var tempContent = that.data.outputContent;
+      tempContent.push('resvMsg:' + resvMsg);
+      that.setData({
+        outputContent: tempContent,
+      })
+    })
+
+    // 监听WebSocket 连接关闭事件
+    socketTask.onClose(close => {
+      var tempContent = that.data.outputContent;
+      tempContent.push('WebSocket 已断开!');
+      that.setData({
+        outputContent: tempContent,
+      })
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  /**
+   * 发送消息
+   */
+  sendMessage:function() {
+    let that = this;
+    var socketTask = that.data.socketTask;
+    var sendMsg = that.data.message;
+
+    socketTask.send({
+      data: sendMsg,
+      success:function() {
+        var tempContent = that.data.outputContent;
+        tempContent.push('sendMsg:' + sendMsg);
+        that.setData({
+          outputContent: tempContent
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+
+  /**
+   * 关闭 WebSocket 连接
+   */
+  closeConnection:function() {
+    let that = this;
+    var socketTask = this.data.socketTask;
+
+    socketTask.close();
   }
 })
